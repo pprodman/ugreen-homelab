376ff96949e099066976fb0ca100ff656fae0222
Pablo RM <pprodman@gmail.com>
2026-09-15 11:58:12 +0200

    [FIX] check schema macro

--- dbt-financial/project/models/macros/generate_schema_name.sql
+++ dbt-financial/project/models/macros/generate_schema_name.sql
@@ -1,10 +1,11 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
+{% macro generate_schema_name(custom_schema_name, node) %}
 
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-        {{ default_schema }}
-    {%- else -%}
+    {{ log("CUSTOM generate_schema_name EJECUTADO | custom=" ~ custom_schema_name ~ " | target=" ~ target.schema, info=True) }}
+
+    {% if custom_schema_name is none %}
+        {{ target.schema }}
+    {% else %}
         {{ custom_schema_name }}
-    {%- endif -%}
+    {% endif %}
 
-{%- endmacro %}
+{% endmacro %}
